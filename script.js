71eb5750cfbf36ba41bdf7eeb5be19e08673eb81
Samantha Zenezi <samanthazenezi@gmail.com>
2022-05-28 20:40:15 -0300

    landing-page-modal

---
+++ script.js
@@ -0,0 +1,11 @@
+function acao(){
+    let modal = document.getElementById('caixa')
+
+    modal.style.display = 'flex';
+}
+
+function fechar(){
+    let modal = document.getElementById('caixa')
+
+    modal.style.display = 'none';
+}
